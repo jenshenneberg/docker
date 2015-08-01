7d7d303f9876b9af76fa181da363303311ca4c7d
Marius Hein <mhein@itsocks.de>
2015-08-01 11:39:17 +0000

    FHEM container

---
+++ armhf-fhem/docker-rootfs/docker-env.sh
@@ -0,0 +1,7 @@
+# Ubuntu default
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export DEBIAN_FRONTEND=noninteractive
+export LANG=C
+export LANGUAGE=en
+export LC_ALL=C
+export TZ=Europe/Berlin
